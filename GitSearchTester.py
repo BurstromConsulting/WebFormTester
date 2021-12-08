bc0f8773706b2657ff7d0a8eda8c2e56a7ccb082
Dennis <eijvergaard.dennis@gmail.com>
2021-12-08 13:21:24 +0100

    Initial Setup with search Field function

---
+++ GitSearchTester.py
@@ -0,0 +1,25 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+class GitSearchTester(unittest.Testcase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+
+    def test_search_github_landing_page(self):
+        driver = self.driver
+        driver.get("https://github.com/")
+        self.assertIn("GitHub", driver.title)
+        searchfield = driver.find_element_by_name("q")
+        searchfield.send_keys("pycon")
+        searchfield.send_keys(Keys.RETURN)
+        assert "No results found." not in driver.page_source
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
