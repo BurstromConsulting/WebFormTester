29bbac226b5812cb5a6bc37315f5ec6a8958c01b
Dennis <eijvergaard.dennis@gmail.com>
2021-12-17 15:17:22 +0100

    First touch-up of files and function calls. Now only instances each browser being called during the test function and runs its associated functions. GitSearchTester is now deprecated due to its functionality being replaced fully by test_webformtester.py

--- locators.py
+++ locators.py
@@ -2,18 +2,17 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators(object):
-    """A class for main page locators. All main page locators should come here"""
-
+    # A class for main page locators. All main page locators should come here
     SEARCH_FIELD = (By.NAME, 'q')
 
 
 class SearchResultsPageLocators(object):
-    """A class for search results locators. All search results locators should
-    come here"""
+    # A class for search results locators. All search results locators should come here
     LANGUAGE_FILTER_LIST = (By.XPATH, '//ul[@class=filter-list small]')
     NAV_MENU_FILTER_LIST = (By.XPATH, '//nav[@class=menu border d-none d-md-block]')
     SEARCH_RESULTS_LIST = (By.CSS_SELECTOR, 'div.codesearch-results')
 
 
 class IssuesPageLocators(object):
+    # Here are the various locators based on the Issues Page of a Repository
     ISSUES_LIST = (By.CSS_SELECTOR, '.js-navigation-container a[id^=issue_]')
--- test_webformtester.py
+++ test_webformtester.py
@@ -11,98 +11,111 @@ from webdriver_manager.firefox import GeckoDriverManager
 import page
 
 
-def edgeWebpageSetup():
-    edge_driver = webdriver.Edge(EdgeChromiumDriverManager().install())
-    return edge_driver
-
-
-def firefoxWebpageSetup():
-    fox_driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-    return fox_driver
-
-
-def chromeWebpageSetup():
-    chrome_driver = webdriver.Chrome(ChromeDriverManager().install())
-    return chrome_driver
-
-
 class TestClassWebform(unittest.TestCase):
     websiteList = ["http://www.github.com"]
     repoList = ["https://github.com/BurstromConsulting/WebFormTester"]
     web_drivers = {}
     websiteName = "GitHub"
-    delay = 2 #seconds
+    delay = 2  # Seconds
     keyword = "webform"
     username = ""
+    searchList = {"keyword": keyword, "username": username}
     # Browser dictionary for future use of making it more dynamical in regards of how many browsers we're testing
     # In this case We're testing Chrome, Edge, Firefox but we're not using the dict for anything currently.
-    browserList = {
-        "chrome": "chrome_driver",
-        "edge": "edge_driver",
-        "firefox": "fox_driver"
-    }
+    browserList = {}
 
-    # @pytest.fixture
-    # def websites(self):
-    #     return self.websiteList
-    #
     @pytest.mark.xdist_group(name="chrome")
-    def test_chrome(self):
-        self.run_test(self.web_drivers.get("chrome"))
+    def test_1chrome(self):
+        self.init_browser("chrome")
+        self.run_test(self.web_drivers["chrome"])
+        self.web_drivers["chrome"].close()
 
     @pytest.mark.xdist_group(name="firefox")
-    def test_firefox(self):
-        self.run_test(self.web_drivers.get("firefox"))
+    def test_2firefox(self):
+        self.init_browser("firefox")
+        self.run_test(self.web_drivers["firefox"])
+        self.web_drivers["firefox"].close()
 
     @pytest.mark.xdist_group(name="edge")
-    def test_edge(self):
-        self.run_test(self.web_drivers.get("edge"))
+    def test_3edge(self):
+        self.init_browser("edge")
+        self.run_test(self.web_drivers["edge"])
+        self.web_drivers["edge"].close()
 
+    # Initiates each browser called in our test
+    def init_browser(self, browsername):
+        if browsername in self.browserList.keys():
+            func = self.browserList.get(browsername)
+            driver = func()
+            self.update_web_drivers(browsername, driver)
+
+    # the list of test functions I'm asking each browser function to run.
     def run_test(self, driver):
         self.website_found(driver)
         self.repo_found(driver)
         self.check_issues(driver)
 
+    def update_web_drivers(self, keyword, driver):
+        self.web_drivers.update({keyword: driver})
+
+    # Looks for a specific repository on GitHubs website, the repositories found are the
     def repo_found(self, driver):
         repo_page = page.MainPage(driver)
-        repo_page.search_text_element = self.keyword
+        repo_page.search_text_element = self.searchList.get("keyword")
         repo_page.go_search()
         try:
-            pageLoad = WebDriverWait(driver, self.delay).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.codesearch-results')))
-            print("Page is ready!")
+            pageLoad = WebDriverWait(driver, self.delay).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, 'div.codesearch-results')))
+            # "Page is ready!"
         except TimeoutException:
             print("Page loading took too much time!")
         search_results_page = page.SearchResultsPage(driver)
         # Verifies that the results page is not empty
         assert search_results_page.is_results_found(), "No results found."
-        links = search_results_page.find_link(self.keyword)
+        links = search_results_page.find_link(self.searchList.get("keyword"))
         assert len(links) != 0, "No Matches to Keyword"
-        search_results_page.click_link(links, self.keyword, self.username)
+        search_results_page.click_link(links, self.searchList.get("keyword"), self.searchList.get("username"))
 
+    # Checks each issue in our Issues Tab for the driver that calls the function.
     def check_issues(self, driver):
         driver.get(self.repoList[0])
         repository_page = page.RepositoryPage(driver)
         repository_page.select_tab("Issues", driver)
         repository_page.click_issue("Closed")
 
+    # One of the tests we're running, this confirms we've ended up on the website we were expecting.
+    # This is to confirm things such as, still having a connection, not having a time-out etc... When loading a page.
     def website_found(self, driver):
         main_page = page.MainPage(driver)
-        # main_page = page.MainPage(self.web_drivers)
         assert main_page.is_title_matches(self.websiteName), "Website not Found"
-        # assert True, "test"
 
+    # When test is called creates each website for each node we're running in our test
     def setUp(self):
-        chrome_driver = chromeWebpageSetup()
-        fox_driver = firefoxWebpageSetup()
-        edge_driver = edgeWebpageSetup()
-        self.web_drivers = {"chrome": chrome_driver, "edge": edge_driver, "firefox": fox_driver}
-        for driver in self.web_drivers:
-            self.web_drivers[driver].get(self.websiteList[0])
-        # self.web_drivers = self.chromeWebpageSetup(self.websiteList[0])
+        self.browserList = {
+            "chrome": self.chromeWebpageSetup,
+            "edge": self.edgeWebpageSetup,
+            "firefox": self.firefoxWebpageSetup
+        }
+
+    def edgeWebpageSetup(self):
+        edge_driver = webdriver.Edge(EdgeChromiumDriverManager().install())
+        edge_driver.get(self.websiteList[0])
+        return edge_driver
+
+    def firefoxWebpageSetup(self):
+        fox_driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+        fox_driver.get(self.websiteList[0])
+        return fox_driver
+
+    def chromeWebpageSetup(self):
+        chrome_driver = webdriver.Chrome(ChromeDriverManager().install())
+        chrome_driver.get(self.websiteList[0])
+        return chrome_driver
 
     def tearDown(self):
-        for driver in self.web_drivers:
-            self.web_drivers[driver].close()
+        print("Done")
+        # for driver in self.web_drivers:
+        #     self.web_drivers[driver].close()
 
 
 if __name__ == '__main__':
